fe09ea7e280d486f2d9a4c7382b94af7697c7846
maureen <maureen.chelangat@student.moringaschool.com>
2023-11-13 12:07:50 +0300

    completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,82 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import AdminNavBar from "./AdminNavBar";
 import QuestionForm from "./QuestionForm";
 import QuestionList from "./QuestionList";
 
 function App() {
   const [page, setPage] = useState("List");
+  const [questions, setQuestions] = useState([]);
+
+  useEffect(() => {
+    fetchQuestions();
+  }, []);
+
+  const fetchQuestions = () => {
+    fetch("http://localhost:4000/questions")
+      .then((response) => response.json())
+      .then((data) => setQuestions(data))
+      .catch((error) => console.error("Error fetching questions:", error));
+  };
+
+  const handleFormSubmit = (newQuestion) => {
+    fetch("http://localhost:4000/questions", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newQuestion),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        setQuestions((prevQuestions) => [...prevQuestions, data]);
+        setPage("List");
+      })
+      .catch((error) => console.error("Error adding question:", error));
+  };
+
+  const handleDelete = (id) => {
+    fetch(`http://localhost:4000/questions/${id}`, {
+      method: "DELETE",
+    })
+      .then(() => {
+        setQuestions((prevQuestions) => prevQuestions.filter((q) => q.id !== id));
+      })
+      .catch((error) => console.error("Error deleting question:", error));
+  };
+
+  const handleUpdateCorrectIndex = (id, newCorrectIndex) => {
+    fetch(`http://localhost:4000/questions/${id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ correctIndex: newCorrectIndex }),
+    })
+      .then(() => {
+        setQuestions((prevQuestions) =>
+          prevQuestions.map((q) => (q.id === id ? { ...q, correctIndex: newCorrectIndex } : q))
+        );
+      })
+      .catch((error) => console.error("Error updating correctIndex:", error));
+  };
 
   return (
     <main>
       <AdminNavBar onChangePage={setPage} />
-      {page === "Form" ? <QuestionForm /> : <QuestionList />}
+      {page === "Form" ? (
+        <QuestionForm onSubmit={handleFormSubmit} />
+      ) : (
+        <QuestionList
+          questions={questions}
+          onDelete={handleDelete}
+          onUpdateCorrectIndex={handleUpdateCorrectIndex}
+        />
+      )}
     </main>
   );
 }
 
 export default App;
+
+
+
--- src/components/QuestionForm.js
+++ src/components/QuestionForm.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 
-function QuestionForm(props) {
+function QuestionForm({ onSubmit }) {
   const [formData, setFormData] = useState({
     prompt: "",
     answer1: "",
@@ -19,7 +19,25 @@ function QuestionForm(props) {
 
   function handleSubmit(event) {
     event.preventDefault();
-    console.log(formData);
+
+    // Validate that essential fields are filled in before submitting
+    if (!formData.prompt || !formData.answer1 || !formData.answer2 || !formData.answer3 || !formData.answer4) {
+      console.error("Please fill in all fields");
+      return;
+    }
+
+    // Call the onSubmit prop to handle the form submission
+    onSubmit(formData);
+
+    // Reset the form after submission
+    setFormData({
+      prompt: "",
+      answer1: "",
+      answer2: "",
+      answer3: "",
+      answer4: "",
+      correctIndex: 0,
+    });
   }
 
   return (
@@ -78,10 +96,10 @@ function QuestionForm(props) {
             value={formData.correctIndex}
             onChange={handleChange}
           >
-            <option value="0">{formData.answer1}</option>
-            <option value="1">{formData.answer2}</option>
-            <option value="2">{formData.answer3}</option>
-            <option value="3">{formData.answer4}</option>
+            <option value="0">Answer 1</option>
+            <option value="1">Answer 2</option>
+            <option value="2">Answer 3</option>
+            <option value="3">Answer 4</option>
           </select>
         </label>
         <button type="submit">Add Question</button>
@@ -91,3 +109,6 @@ function QuestionForm(props) {
 }
 
 export default QuestionForm;
+
+
+
